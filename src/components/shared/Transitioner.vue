93e8cb0ab0c148ae4cd6d36a3a5cce1583a70168
Hamid Reza Shakibi <lost.gost.1999@gmail.com>
2025-03-04 12:33:49 +0330

    wip: add animation base component

---
+++ src/components/shared/Transitioner.vue
@@ -0,0 +1,29 @@
+<template>
+    <Transition
+        @onBeforeEnter="onBeforeEnter"
+        @onBeforeAppear="onBeforeAppear"
+        @onEnter="onEnter"
+        @onAppear="onAppear"
+        @onLeave="onLeave"
+        @onEnterCancelled="onEnterCancelled"
+        @onAppearCancelled="onAppearCancelled"
+        @onLeaveCancelled="onLeaveCancelled"
+        appear
+        :css="false"
+        ><slot
+    /></Transition>
+</template>
+
+<script lang="ts" setup>
+const onBeforeEnter = (el: Element) => {};
+const onBeforeAppear = (el: Element) => {};
+const onEnter = (el: Element, done: () => void) => {};
+const onAppear = (el: Element, done: () => void) => {};
+const onLeave = (
+    el: Element & { _isLeaving?: boolean; _enterCancelled?: boolean },
+    done: () => void
+) => {};
+const onEnterCancelled = (el: Element) => {};
+const onAppearCancelled = (el: Element) => {};
+const onLeaveCancelled = (el: Element) => {};
+</script>
